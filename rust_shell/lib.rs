3bd701c60fbeb3306bce3ff7693b5cdcfbb21ce7
czeng <chzeng18@gmail.com>
2023-11-30 01:46:42 -0500

    fixed weird print ordering

--- rust_shell/lib.rs
+++ rust_shell/lib.rs
@@ -1,9 +1,10 @@
-use std::io;
+use std::io::{self, Write};
 use std::process::Command;
 
 pub fn run_interface() {
     loop {
         print!("> ");
+        flush();
         let mut com = String::new();
         io::stdin()
             .read_line(&mut com)
@@ -16,7 +17,10 @@ pub fn run_interface() {
             .args(&args[1..])
             .spawn()
             .expect("failed to execute process");
-        let ecode = process.wait().expect("failed to wait on child");
-        print!("{}", ecode);
+        let _ = process.wait().expect("failed to wait on child");
     }
+}
+
+fn flush() {
+    let _ = io::stdout().flush();
 }
